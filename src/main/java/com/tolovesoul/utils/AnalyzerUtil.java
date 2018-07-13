a778dbedcbe4e603eb6383dfe876109fc521a7b4
yuanyue <hensigui@outlook.com>
2018-07-13 17:00:38 +0800

    增加停用词列表，提高猜想精度

--- src/main/java/com/tolovesoul/utils/AnalyzerUtil.java
+++ src/main/java/com/tolovesoul/utils/AnalyzerUtil.java
@@ -1,13 +1,20 @@
 package com.tolovesoul.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+import com.tolovesoul.TlsSearchClientApplication;
+
 
 /**
  * lucene分词工具
@@ -16,6 +23,27 @@ import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
  * @date 2018年7月12日
  */
 public class AnalyzerUtil {
+	/**
+	 * 自定义中文停用词
+	 */
+	public static List<String> stopWords=new ArrayList<String>();
+	
+	/**
+	 * 初始化停用词
+	 */
+	static {
+		BufferedReader wordReader=new BufferedReader(new InputStreamReader(TlsSearchClientApplication.class.getResourceAsStream("/stopWord.txt")));
+		String word="";
+		try {
+			while((word=wordReader.readLine())!=null){
+				stopWords.add(word.trim());
+				//System.out.println("停用词："+word);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
     /**
      *
@@ -32,7 +60,9 @@ public class AnalyzerUtil {
             //保存相应词汇
             CharTermAttribute cta = stream.addAttribute(CharTermAttribute.class);
             while(stream.incrementToken()){
-                System.out.print("[" + cta + "]");
+            	if(!stopWords.contains(cta.toString())) {
+            		System.out.print("[" + cta + "]");
+                }
             }
             System.out.println();
         } catch (IOException e) {
@@ -55,10 +85,12 @@ public class AnalyzerUtil {
             //保存相应词汇
             CharTermAttribute cta = stream.addAttribute(CharTermAttribute.class);
             while(stream.incrementToken()){
-                if(termsMap.containsKey(cta.toString())) {
-                	termsMap.put(cta.toString(), termsMap.get(cta.toString())+1);//已存在词条，词条数加1
-                }else {
-                	termsMap.put(cta.toString(), 1);//不存在词条，初始化
+            	if(!stopWords.contains(cta.toString())) {//非停用词加入计数
+            		if(termsMap.containsKey(cta.toString())) {
+                    	termsMap.put(cta.toString(), termsMap.get(cta.toString())+1);//已存在词条，词条数加1
+                    }else {
+                    	termsMap.put(cta.toString(), 1);//不存在词条，初始化
+                    }
                 }
             }
         } catch (IOException e) {
@@ -69,9 +101,9 @@ public class AnalyzerUtil {
     }
     
     public static void main(String[] args) {
-        Analyzer aly1 = new SmartChineseAnalyzer();
+    	Analyzer aly1 = new SmartChineseAnalyzer();
         
-        String str = "hello kim,I am dennisit,我是 中国人,my email is dennisit@163.com, and my QQ is 1325103287";
+        String str = "hello kim,I am dennisit,我是 中国的人，来自四川和湖南的朋友,my email is dennisit@163.com, and my QQ is 1325103287";
         
         AnalyzerUtil.displayToken(str, aly1);
     }
